74320632e9c14e10a7fd0dbbcbf08feae20e2750
Dayton <hello@daytonm.com>
2017-12-08 16:38:59 -0800

    Building out dashboard and employee pages
    Stats, styling, and structure

--- client/routes.js
+++ client/routes.js
@@ -7,10 +7,33 @@ module.exports = function(app) {
         .getAddresses()
         .then(result => {
             let renderData = {
-                employees: result.data
+                employees: result.data,
+                active: { dashboard: true }
             };
 
             res.render('dashboard', renderData);
+        })
+        .catch(err => console.log(err));
+    });
+
+    app.get('/employees', (req, res) => {
+        API
+        .getAddresses()
+        .then(result => {
+            let renderData = {
+                employees: result.data,
+                count: result.data.length,
+                total: 0,
+                active: { employees: true }
+            };
+
+            // 2+2 is 4 - quik mafs
+            for(let s = 0; s < result.data.length; s++)
+            {
+                renderData.total += result.data[s].salary;
+            }
+
+            res.render('employees', renderData);
         })
         .catch(err => console.log(err));
     });
